f081589b400bf60bc2db405bea0dae2ea4de11f7
Chris Garvie <cg2035@hw.ac.uk>
2023-10-04 16:47:19 +0100

    added arrays and objects in javaScript

--- practicesite/scriptPractice.js
+++ practicesite/scriptPractice.js
@@ -1,6 +1,20 @@
 let sum = 0;
-let arr = [1, 2, 3];
-for (let i=1; i<arr.length; i++) { // classic loop
+let arr = [1, 2, 3, 4, 5];
+for (let i=0; i<arr.length; i++) { // classic loop
     sum += arr[i];
 }
 alert( sum );
+
+let colours = new Array("Blue","Black","Pink","White","Red","Turquoise");
+alert(colours);
+
+function robotMaker(model, make, manufactureYear){
+    this.model = model;
+    this.make = make;
+    this.manufactureYear = manufactureYear;
+
+}
+let robotA = new robotMaker("Mark VII", "ASW", 1996);
+let robotL = new robotMaker("Mark V", "ASW", 1994);
+alert([robotA.model, robotA.make, robotA.manufactureYear]);
+alert([robotL.model, robotL.make, robotL.manufactureYear]);
